fe0853f6041f298930ca5f796a101197972efa16
Ryan Cragun <ryan@chef.io>
2019-10-10 15:14:31 -0600

    [studio] update studio and protoc shells scripts for go modules
    * Update all go related studio functions for go modules with a vendor
    directory.
    * Update all gRPC proto compilation scripts to also support go
    mod vendoring.
    
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- components/automate-grpc/scripts/grpc.sh
+++ components/automate-grpc/scripts/grpc.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-set -x
-GOPATH=$(go env GOPATH)
 
-protoc --go_out=logtostderr=true:$GOPATH/src \
+printf 'GEN: %s\n' components/automate-grpc/protoc-gen-policy/api/*.proto
+protoc --go_out=logtostderr=true,paths=source_relative:/src \
   components/automate-grpc/protoc-gen-policy/api/*.proto
-protoc --go_out=logtostderr=true:$GOPATH/src \
+
+printf 'GEN: %s\n' components/automate-grpc/protoc-gen-policy/iam/*.proto
+protoc --go_out=logtostderr=true,paths=source_relative:/src \
   components/automate-grpc/protoc-gen-policy/iam/*.proto
-protoc --go_out=plugins=grpc:$GOPATH/src \
-  --policy_out=logtostderr=true:$GOPATH/src \
+
+printf 'GEN: %s\n' lib/grpc/debug/debug_api/*.proto
+protoc --go_out=plugins=grpc,paths=source_relative:/src \
+  --policy_out=logtostderr=true,paths=source_relative:/src \
   lib/grpc/debug/debug_api/*.proto
 
-protoc --go_out=logtostderr=true:$GOPATH/src \
+printf 'GEN: %s\n' components/automate-grpc/protoc-gen-a2-config/api/**/*.proto
+protoc --go_out=logtostderr=true,paths=source_relative:/src \
   components/automate-grpc/protoc-gen-a2-config/api/**/*.proto
