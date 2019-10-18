af8ff4434a1226682a67d27cfe43f03744aaeeff
Ryan Cragun <ryan@chef.io>
2019-10-18 15:15:30 -0600

    [ci] attempt to fix CI errors
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- components/automate-grpc/scripts/grpc.sh
+++ components/automate-grpc/scripts/grpc.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+set -e
+
 printf 'GEN: %s\n' components/automate-grpc/protoc-gen-policy/api/*.proto
-protoc --go_out=logtostderr=true,paths=source_relative:/src \
+protoc -I /src --go_out=logtostderr=true,paths=source_relative:/src \
   components/automate-grpc/protoc-gen-policy/api/*.proto
 
 printf 'GEN: %s\n' components/automate-grpc/protoc-gen-policy/iam/*.proto
-protoc --go_out=logtostderr=true,paths=source_relative:/src \
+protoc -I /src --go_out=logtostderr=true,paths=source_relative:/src \
   components/automate-grpc/protoc-gen-policy/iam/*.proto
 
 printf 'GEN: %s\n' lib/grpc/debug/debug_api/*.proto
-protoc --go_out=plugins=grpc,paths=source_relative:/src \
+protoc -I /src --go_out=plugins=grpc,paths=source_relative:/src \
   --policy_out=logtostderr=true,paths=source_relative:/src \
   lib/grpc/debug/debug_api/*.proto
 
-printf 'GEN: %s\n' components/automate-grpc/protoc-gen-a2-config/api/**/*.proto
-protoc --go_out=logtostderr=true,paths=source_relative:/src \
-  components/automate-grpc/protoc-gen-a2-config/api/**/*.proto
+printf 'GEN: %s\n' components/automate-grpc/protoc-gen-a2-config/api/a2conf/*.proto
+protoc -I /src --go_out=logtostderr=true,paths=source_relative:/src \
+  components/automate-grpc/protoc-gen-a2-config/api/a2conf/*.proto
